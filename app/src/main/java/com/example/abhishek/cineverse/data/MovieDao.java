2658bbe593ba5d327bde28294c0dae39bb3e1b09
AbhishekChd <acechd97@gmail.com>
2018-07-09 03:30:20 +0530

    Created a basic movie DAO

---
+++ app/src/main/java/com/example/abhishek/cineverse/data/MovieDao.java
@@ -0,0 +1,29 @@
+package com.example.abhishek.cineverse.data;
+
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+
+import com.example.abhishek.cineverse.models.Movie;
+
+/**
+ * Movie DAO stores and retrieves {@link Movie} to the database using {@link android.arch.persistence.room.Room}
+ */
+public interface MovieDao {
+
+    /**
+     * Insert an single instance of {@link Movie} to the database
+     *
+     * @param movie The {@link Movie} to insert in database
+     */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertMovie(Movie movie);
+
+    /**
+     * Insert an Array of {@link Movie} to the database
+     *
+     * @param movies The {@link Movie} array to insert in database
+     */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void bulkMovieInsert(Movie... movies);
+
+}
