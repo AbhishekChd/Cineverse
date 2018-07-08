0b6edbbf07268e28987128ac62a124ec19822471
AbhishekChd <acechd97@gmail.com>
2018-07-09 03:36:09 +0530

    Created movie database

---
+++ app/src/main/java/com/example/abhishek/cineverse/data/MovieDatabase.java
@@ -0,0 +1,33 @@
+package com.example.abhishek.cineverse.data;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import com.example.abhishek.cineverse.models.Movie;
+
+@Database(entities = {Movie.class}, version = 1, exportSchema = false)
+public abstract class MovieDatabase extends RoomDatabase {
+
+    private static final String LOG_TAG = MovieDatabase.class.getSimpleName();
+    private static final String DATABASE_NAME = "cineverse";
+
+    private static final Object LOCK = new Object();
+    private static MovieDatabase sInstance;
+
+    public static MovieDatabase getInstance(Context context) {
+        if (sInstance == null) {
+            synchronized (LOCK) {
+                sInstance = Room.databaseBuilder(
+                        context,
+                        MovieDatabase.class,
+                        DATABASE_NAME).build();
+            }
+        }
+        return sInstance;
+    }
+
+    // Associated Movie DOA for database
+    public abstract MovieDao movieDao();
+}
